98dc64284362bfdf48e4887df274d1881f8b8b21
TendTo <casablancaernesto@gmail.com>
2025-05-27 14:28:52 +0100

    feat: add more dataclass to ros and viceversa serialisation (wip)

--- python/fsds/ros.py
+++ python/fsds/ros.py
@@ -1,5 +1,6 @@
 from roslibpy import Ros, Topic
-from typing import Any
+from roslibpy.ros2 import Header
+from typing import Any, Callable, TypeVar, TypedDict
 from .types import *
 
 obj_to_msg_type = {
@@ -7,9 +8,9 @@ obj_to_msg_type = {
     Quaternionr: "geometry_msgs/Quaternion",
     Pose: "geometry_msgs/Pose",
     GeoPoint: "geographic_msgs/GeoPoint",
-    # ImageRequest
-    # ImageResponse
-    # CarControls
+    # ImageRequest # NOT NECESSARY
+    ImageResponse: "sensor_msgs/Image",
+    CarControls: "newcastle_racing_ai_msgs/ControlCommand",
     # KinematicsState
     # EnvironmentState
     # CollisionInfo
@@ -17,12 +18,21 @@ obj_to_msg_type = {
     Point2D: "geometry_msgs/Point",
     # RefereeState
     # ProjectionMatrix
-    # LidarData
-    # ImuData
+    LidarData: "sensor_msgs/PointCloud2",
+    ImuData: "sensor_msgs/Imu",
     # GnssReport
     # GpsData
     # GroundSpeedSensorData
 }
+
+T = TypeVar("T")
+
+
+class Field(TypedDict):
+    name: str
+    offset: int
+    datatype: int
+    count: int
 
 
 class RosBridgeClient:
@@ -35,6 +45,7 @@ class RosBridgeClient:
         headers: "dict | None" = None,
     ):
         self._client = Ros(host=host, port=port, is_secure=is_secure, headers=headers)
+        self._subscribed_topics: "list[Topic]" = []
 
     def get_topic(
         self,
@@ -59,23 +70,103 @@ class RosBridgeClient:
             reconnect_on_close,
         )
 
-    def publish(self, topic: str, msg: "Vector3r | Quaternionr | Pose | GeoPoint"):
+    @classmethod
+    def dataclass_to_ros_msg(
+        cls, msg: "Vector3r | Quaternionr | Pose | GeoPoint | ImageResponse"
+    ) -> dict:
         if type(msg) not in obj_to_msg_type:
             raise ValueError(
                 f"{type(msg)} not among the allowed messages: {obj_to_msg_type.keys()}"
             )
         # For some reason these datclasses append "_val" to their fields. We muse remove it
-        if type(msg) in (Vector3r, Quaternionr):
-            d = {k.removesuffix("_val"): v for k, v in msg.to_dict().items()}
-        else:
-            d = msg.to_dict()
-        Topic(self._client, topic, obj_to_msg_type[type(msg)]).publish(d)
-        print("FEST")
+        if isinstance(msg, (Vector3r, Quaternionr)):
+            return {k.removesuffix("_val"): v for k, v in msg.to_dict().items()}
+        if isinstance(msg, ImageResponse):
+            # Handle the more complex case of ImageResponse
+            return {
+                "header": Header(),
+                "height": msg.height,
+                "width": msg.width,
+                "encoding": "bgr8",
+                "step": msg.width * 3,
+                "data": msg.image_data_uint8,
+                "is_bigendian": False,
+            }
+        if isinstance(msg, ImuData):
+            avc = msg.sigma_arw**2
+            lac = msg.sigma_vrw**2
+            # Why is it like this? No idea, check airsim_ros_wrapper
+            return {
+                "header": Header(),
+                "orientation": cls.dataclass_to_ros_msg(msg.orientation),
+                "angular_velocity": cls.dataclass_to_ros_msg(msg.angular_velocity),
+                "linear_acceleration": cls.dataclass_to_ros_msg(
+                    msg.linear_acceleration
+                ),
+                "angular_velocity_covariance": [avc, 0, 0, 0, avc, 0, 0, 0, avc],
+                "linear_acceleration_covariance": [lac, 0, 0, lac, 0, 0, 0, lac],
+            }
+        if isinstance(msg, LidarData):
+            fields: list[Field] = []
+            for i in range(0, 3 * 4, 4):
+                fields.append(
+                    {
+                        "name": ("x" if i == 0 else ("y" if i == 4 else "z")),
+                        "offset": i,
+                        "count": 1,
+                        "datatype": 7, # Datatype FLOAT32 https://docs.ros.org/en/noetic/api/sensor_msgs/html/msg/PointField.html
+                    }
+                )
+            offset = 3 * 4
+            return {
+                "header": Header(),
+                "height": 1,
+                "width": len(msg.point_cloud),
+                "fields": fields,
+                "is_bigendian": False,
+                "point_step": offset,
+                "row_step": offset * len(msg.point_cloud),
+                "is_dense": True,
+                "data": msg.point_cloud
+            }
+        return msg.to_dict()
+
+    def publish(
+        self,
+        topic: str,
+        msg: "Vector3r | Quaternionr | Pose | GeoPoint | ImageResponse",
+    ):
+        Topic(self._client, topic, obj_to_msg_type[type(msg)], latch=True).publish(
+            self.dataclass_to_ros_msg(msg)
+        )
+
+    @staticmethod
+    def ros_msg_to_dataclass(msg_type: type[T], msg: dict) -> T:
+        print(f"ros_msg_to_dataclass: {msg_type}, {msg}")
+        if False:
+            pass
+        return msg_type(**msg)
+
+    def subscribe(
+        self, topic: str, msg_type: type[T], cb: "Callable[[T], None]"
+    ) -> int:
+        self._subscribed_topics.append(
+            Topic(self._client, topic, obj_to_msg_type[msg_type])
+        )
+        self._subscribed_topics[-1].subscribe(
+            lambda msg: cb(self.ros_msg_to_dataclass(msg_type, msg))
+        )
+        return len(self._subscribed_topics) - 1
+
+    def unsubscribe(self, topic_idx: int):
+        self._subscribed_topics[topic_idx].unsubscribe()
 
     def __enter__(self):
+        self._subscribed_topics.clear()
         self._client.run()
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self._subscribed_topics.clear()
         if self._client.is_connected:
             self._client.close()
