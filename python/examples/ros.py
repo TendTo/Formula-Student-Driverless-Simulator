e4be49389e50cc123124c3ed73cf402109ada0d1
TendTo <casablancaernesto@gmail.com>
2025-05-27 14:28:04 +0100

    feat: improve example (wip)

--- python/examples/ros.py
+++ python/examples/ros.py
@@ -2,27 +2,43 @@ import time
 import fsds
 from fsds.ros import RosBridgeClient
 
-# connect to the AirSim simulator 
-client = fsds.FSDSClient()
 
-# Check network connection
-client.confirmConnection()
+def show_lidar(lidar_data: "fsds.LidarData"):
+    import numpy as np
+    import matplotlib.pyplot as plt
+    plt.switch_backend("TkAgg")
+    data = np.array(lidar_data.point_cloud)
+    x = data[::3]
+    y = data[1::3]
+    z = data[2::3]
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(x, y, z)
+    plt.show()
 
-# After enabling api controll only the api can controll the car. 
-# Direct keyboard and joystick into the simulator are disabled.
-# If you want to still be able to drive with the keyboard while also 
-# controll the car using the api, call client.enableApiControl(False)
-client.enableApiControl(False)
+
+def main():
+    TIME_STEP = 5
+
+    # connect to the AirSim simulator
+    client = fsds.FSDSClient()
+
+    # Check network connection
+    client.confirmConnection()
+
+    # After enabling api controll only the api can controll the car.
+    # Direct keyboard and joystick into the simulator are disabled.
+    # If you want to still be able to drive with the keyboard while also
+    # controll the car using the api, call client.enableApiControl(False)
+    client.enableApiControl(False)
+
+    with RosBridgeClient() as ros_client:
+        while True:
+            ros_client.publish("/nrfai/imu", client.getImuData())
+            ros_client.publish("/nrfai/lidar", client.getLidarData())
+            ros_client.publish("/nrfai/camera", client.simGetImages())
+            time.sleep(TIME_STEP)
 
 
-with RosBridgeClient() as ros_client:
-    ros_client.publish("/fest", msg=fsds.Vector3r(x_val=31, y_val=2, z_val=22))
-
-exit(1)
-
-while True:
-    lidardata = client.getLidarData()
-    print(lidardata)
-    print(lidardata.to_dict())
-    time.sleep(2)
-
+if __name__ == "__main__":
+    main()
